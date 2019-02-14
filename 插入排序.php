b8d3a5318e8c745a1cac103ed7de5e93d74ce210
TonyHe <he@holptech.com>
2019-02-14 20:33:03 +0800

    14-2-2019

---
+++ 插入排序.php
@@ -0,0 +1,21 @@
+<?php
+/*
+ * 插入排序
+ */
+
+$array = array(1,3,5,1,2,35,6,123);
+$count_array = count($array);
+
+for($i=1;$i<$count_array;$i++){
+    $index = $i - 1;
+    $current = $array[$i];
+    while($index>=0 && $array[$index] > $current){
+        $array[$index+1] = $array[$index];
+        $array[$index] = $current;
+        $index--;
+    }
+}
+
+var_dump($array);
+
+?>
