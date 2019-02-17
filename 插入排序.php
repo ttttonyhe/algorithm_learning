e3084646e62e2db5611a05a27bb1ade77df1462a
TonyHe <he@holptech.com>
2019-02-17 20:52:41 +0800

    17-2-2019

--- 插入排序.php
+++ 插入排序.php
@@ -10,7 +10,7 @@ for($i=1;$i<$count_array;$i++){
     $index = $i - 1;
     $current = $array[$i];
     while($index>=0 && $array[$index] > $current){
-        $array[$index+1] = $array[$index];
+        $array[$index+1] = $array[$index]; //不用担心覆盖，当前位置之前的所有有序数组元素都比current大，也就是说有序数组最后一位(最先比较的那一位的键值已经加1了)
         $index--;
     }
     $array[$index+1] = $current;
